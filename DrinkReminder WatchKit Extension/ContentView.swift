cb38472a7564b103cbba9ef756bff452ed4e8d12
Cihan Hasanoglu <cihan.hasanoglu@icloud.com>
2020-10-15 22:32:28 +0200

    Initial Commit

---
+++ DrinkReminder WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrinkReminder WatchKit Extension
+//
+//  Created by Cihan Hasanoglu on 15.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrinkReminder WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  DrinkReminder WatchKit Extension
+//
+//  Created by Cihan Hasanoglu on 15.10.20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
