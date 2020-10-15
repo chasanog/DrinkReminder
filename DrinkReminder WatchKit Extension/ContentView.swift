e68ed3be141c6780156d3d69819a3f4985d4981a
Cihan Hasanoglu <cihan.hasanoglu@icloud.com>
2020-10-15 23:07:15 +0200

    added Picker and button for selecting the amount of Water

--- DrinkReminder WatchKit Extension/ContentView.swift
+++ DrinkReminder WatchKit Extension/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedAmount = 0
+    private var max = 250
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        
+        VStack(spacing: 5) {
+            Picker(selection: $selectedAmount, label: Text("How much did you drink"), content: /*@START_MENU_TOKEN@*/{
+                ForEach(0 ..< max) {
+                    Text("\($0) ml")
+                }
+            }/*@END_MENU_TOKEN@*/)
+            Button(action:{}) {
+                Text("Drink!")
+            }.background(Color.blue)
+        }
     }
 }
 
